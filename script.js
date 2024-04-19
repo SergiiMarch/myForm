ae0f7916fd0b576b661a854cb464fa9f979c34ae
Sergii Marchenko <marikmarchelo@gmail.com>
2024-04-19 09:40:33 +0300

    new

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello word");
+const refs = {
+  form: document.getElementById("form"),
+  amount: document.getElementById("amount"),
+  total: document.getElementById("total"),
+};
+
+refs.form.addEventListener("input", handleFormInput);
+
+function handleFormInput(e) {
+  console.log(e.target);
+}
